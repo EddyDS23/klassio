83180388a3cca7ee251fac81abe1970985443f97
BUSSEDU <lalorivera1216@gmail.com>
2026-09-05 22:48:12 -0600

    feat(word-search): register word search routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\WordsearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('teacher')->name('teacher.wordsearches.')->group(function () {
+    Route::get('wordsearches/create', [WordsearchController::class, 'create'])->name('create');
+    Route::post('wordsearches', [WordsearchController::class, 'store'])->name('store');
+});
+
+Route::get('wordsearches/{wordsearch}', [WordsearchController::class, 'show'])->name('wordsearches.show');
+Route::get('wordsearches/{wordsearch}/play', [WordsearchController::class, 'play'])->name('wordsearches.play');
+Route::post('wordsearches/{wordsearch}/answer', [WordsearchController::class, 'answer'])->name('wordsearches.answer');
