e343ec3e434ec5d374100565ec8e45a9c312ab8b
BUSSEDU <lalorivera1216@gmail.com>
2026-09-05 22:55:33 -0600

    feat(matching): register matching routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\MatchingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('teacher')->name('teacher.matchings.')->group(function () {
+    Route::get('matchings/create', [MatchingController::class, 'create'])->name('create');
+    Route::post('matchings', [MatchingController::class, 'store'])->name('store');
+});
+
+Route::get('matchings/{matching}', [MatchingController::class, 'show'])->name('matchings.show');
+Route::get('matchings/{matching}/play', [MatchingController::class, 'play'])->name('matchings.play');
+Route::post('matchings/{matching}/answer', [MatchingController::class, 'answer'])->name('matchings.answer');
