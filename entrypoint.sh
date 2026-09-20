3949d2bf7e13b679795cb68f2c61763d025b62e9
Waxho <carrilloedgar408@gmail.com>
2026-09-19 20:18:53 -0600

    feat: add seeders in entrypoint and unless-stopped in db

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,7 @@ if [ "$1" = 'php-fpm' ] || [ -z "$1" ]; then
 
     echo "Ejecutando migraciones..."
     php artisan migrate --force
+    php artisan db:seed
 
     echo "Ajustando permisos..."
     chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
